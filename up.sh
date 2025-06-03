bcf5b626c247dcaf692e1e4a40466e9ef39389bc
srxrt <str9703@gmail.com>
2025-06-04 06:02:22 +0900

    fix: create api-gateway service, modify up.sh

--- up.sh
+++ up.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 docker compose stop
-docker compose rm -f patient-service billing-service analytics-service
-docker rmi -f patient-service billing-service analytics-service
+docker compose rm -f patient-service billing-service analytics-service api-gateway
+docker rmi -f patient-service billing-service analytics-service api-gateway
+
+docker compose up -d patient-service
 docker compose up -d
