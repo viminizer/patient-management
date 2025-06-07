bd30b6856667df27ec1bdb4af915d5fd945c8f8d
srxrt <str9703@gmail.com>
2025-06-08 03:09:16 +0900

    fix: modify auth service container

--- up.sh
+++ up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker compose stop
-docker compose rm -f auth-service
-docker rmi -f auth-service
+docker compose rm -f auth-service api-gateway
+docker rmi -f auth-service api-gateway
 
-docker compose up -d patient-service
+docker compose up -d patient-service auth-service
 docker compose up -d
