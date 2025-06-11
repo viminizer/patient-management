512c78a0fbaf52954a97e6fbb95af7d699a6f101
srxrt <str9703@gmail.com>
2025-06-12 07:51:45 +0900

    chore: update submodules and refine up.sh script

--- up.sh
+++ up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker compose stop
-docker compose rm -f auth-service api-gateway
-docker rmi -f auth-service api-gateway
-
+# docker compose rm -f auth-service api-gateway billing-service analytics-service patient-service
+# docker rmi -f auth-service api-gateway billing-service analytics-service patient-service
+# docker compose build --no-cache && docker compose up --force-recreate
 docker compose up -d patient-service auth-service
 docker compose up -d
